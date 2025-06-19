ff510aea1cbcd13ae31ca20c2a18730ab971623c
R I P Skillan <ripskillan312004@gmail.com>
2025-06-19 19:23:06 +0530

    Graph

---
+++ src/Components/Graph.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import CanvasJSReact from '@canvasjs/react-charts';
+import { InView } from 'react-intersection-observer';
+
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showChart: false
+    };
+  }
+
+  render() {
+    const options = {
+      animationEnabled: true,
+      exportEnabled: true,
+      theme: 'light2',
+      title: {
+        text: 'Just Another Random Graph'
+      },
+      axisY: {
+        includeZero: true
+      },
+      data: [{
+        type: 'column',
+        indexLabelFontColor: '#5A5757',
+        indexLabelPlacement: 'outside',
+        dataPoints: [
+          { x: 10, y: 71 },
+          { x: 20, y: 55 },
+          { x: 30, y: 50 },
+          { x: 40, y: 65 },
+          { x: 50, y: 71 },
+          { x: 60, y: 68 },
+          { x: 70, y: 38 },
+          { x: 80, y: 92, indexLabel: 'Highest' },
+          { x: 90, y: 54 },
+          { x: 100, y: 60 },
+          { x: 110, y: 21 },
+          { x: 120, y: 49 },
+          { x: 130, y: 36 }
+        ]
+      }]
+    };
+
+    return (
+      <InView
+        as="div"
+        onChange={(inView) => {
+          if (inView) this.setState({ showChart: true });
+        }}
+      >
+        {this.state.showChart && (
+          <CanvasJSChart options={options} />
+        )}
+      </InView>
+    );
+  }
+}
+
+export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import BrandKitSelector from "../Components/CardBuild";
 import FluidObject from "../Components/FluidObject";
+import AnimatedBarGraph from "../Components/Graph";
 import Parallax from "../Components/Parallax";
 import BSSSection from "../Components/Services";
 import { motion, useScroll, useSpring } from "framer-motion";
@@ -17,23 +18,26 @@ export default function Home() {
 
   return (
     <>
-      <motion.div
-        id="scroll-indicator" className="z-20"
-        style={{
-            scaleX: scrollYProgress,
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            height: 10,
-            originX: 0,
-            backgroundColor: "#ff0088",
-        }}
-      />
-      <Parallax />
-      <FluidObject />
-      <BrandKitSelector />
-      <BSSSection />
+      <div className="py-10">
+        <motion.div
+          id="scroll-indicator" className="z-20"
+          style={{
+              scaleX: scrollYProgress,
+              position: "fixed",
+              top: 0,
+              left: 0,
+              right: 0,
+              height: 10,
+              originX: 0,
+              backgroundColor: "#ff0088",
+          }}
+        />
+        <Parallax />
+        <FluidObject />
+        <BrandKitSelector />
+        <BSSSection />
+        <AnimatedBarGraph />
+      </div>
     </>
   )
 }
